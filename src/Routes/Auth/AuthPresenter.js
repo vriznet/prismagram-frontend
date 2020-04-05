38eb24593393448f58297d89cdab4fdc0b50a2eb
Shin Woo Lee <vriz@vriz.net>
2020-04-05 10:54:29 +0900

    5.4 requestSecret Mutation and Refactor

---
+++ src/Routes/Auth/AuthPresenter.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+import Input from '../../Components/Input';
+import Button from '../../Components/Button';
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Box = styled.div`
+  ${(props) => props.theme.whiteBox}
+  border-radius: 0px;
+  width: 100%;
+  max-width: 350px;
+`;
+
+const StateChanger = styled(Box)`
+  text-align: center;
+  padding: 20px 0px;
+`;
+
+const Link = styled.span`
+  color: ${(props) => props.theme.blueColor};
+  cursor: pointer;
+`;
+
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100%;
+    input {
+      width: 100%;
+      &:not(:last-child) {
+        margin-bottom: 5px;
+      }
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
+`;
+
+export default ({
+  action,
+  setAction,
+  username,
+  firstName,
+  lastName,
+  email,
+  onLogin,
+}) => (
+  <Wrapper>
+    <Form>
+      {action === 'logIn' ? (
+        <form onSubmit={onLogin}>
+          <Input {...email} placeholder={'Email'} type="email" />
+          <Button text={'Log In'} />
+        </form>
+      ) : (
+        <form onSubmit={onLogin}>
+          <Input {...firstName} placeholder={'First name'} />
+          <Input {...lastName} placeholder={'Last name'} />
+          <Input {...username} placeholder={'Username'} />
+          <Input {...email} placeholder={'Email'} type="email" />
+          <Button text={'Sign Up'} />
+        </form>
+      )}
+    </Form>
+    <StateChanger>
+      {action === 'logIn' ? (
+        <React.Fragment>
+          Don't have an account?{' '}
+          <Link onClick={() => setAction('signUp')}>Sign up</Link>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          Have an account?{' '}
+          <Link onClick={() => setAction('logIn')}>Log in</Link>
+        </React.Fragment>
+      )}
+    </StateChanger>
+  </Wrapper>
+);
---
+++ src/Routes/Auth/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import AuthContainer from './AuthContainer';
+
+export default () => <AuthContainer />;
